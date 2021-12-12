6041b89c256b1897b8b3df26469dba8566b22225
brianb12321 <brian.jx.b@gmail.com>
2021-12-11 21:17:12 -0600

    Initialized repository

---
+++ YoutubeDownloader/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace YoutubeDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_url = new System.Windows.Forms.TextBox();
+            this.rtb_output = new System.Windows.Forms.RichTextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.lb_progress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // tb_url
+            // 
+            this.tb_url.Location = new System.Drawing.Point(171, 19);
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(283, 23);
+            this.tb_url.TabIndex = 1;
+            // 
+            // rtb_output
+            // 
+            this.rtb_output.Location = new System.Drawing.Point(12, 91);
+            this.rtb_output.Name = "rtb_output";
+            this.rtb_output.ReadOnly = true;
+            this.rtb_output.Size = new System.Drawing.Size(532, 116);
+            this.rtb_output.TabIndex = 5;
+            this.rtb_output.Text = "";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(469, 242);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 6;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Enabled = false;
+            this.btn_cancel.Location = new System.Drawing.Point(379, 242);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 7;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 213);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(487, 23);
+            this.progress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progress.TabIndex = 8;
+            // 
+            // lb_progress
+            // 
+            this.lb_progress.AutoSize = true;
+            this.lb_progress.Location = new System.Drawing.Point(505, 221);
+            this.lb_progress.Name = "lb_progress";
+            this.lb_progress.Size = new System.Drawing.Size(39, 15);
+            this.lb_progress.TabIndex = 9;
+            this.lb_progress.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 276);
+            this.Controls.Add(this.lb_progress);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.rtb_output);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Youtube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.RichTextBox rtb_output;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label lb_progress;
+    }
+}
+
---
+++ YoutubeDownloader/MainForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader
+{
+    public partial class MainForm : Form
+    {
+        CancellationTokenSource _cancelTokenSource;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_start_Click(object sender, EventArgs e)
+        {
+            rtb_output.Clear();
+            progress.Value = 0;
+            try
+            {
+                btn_cancel.Enabled = true;
+                btn_start.Enabled = false;
+                rtb_output.AppendText("Downloading Video Data." + Environment.NewLine);
+                YoutubeClient client = new YoutubeClient();
+                var video = await client.Videos.GetAsync(VideoId.Parse(tb_url.Text));
+                var videoStreamInfo = await client.Videos.Streams.GetManifestAsync(video.Id);
+                var audioStream = videoStreamInfo.GetAudioOnlyStreams().GetWithHighestBitrate();
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Save Audio File";
+                sfd.Filter = $"{audioStream.Container.Name}|*.{audioStream.Container.Name}|MP3 File (*.mp3)|*.mp3";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    progress.Maximum = 100;
+                    _cancelTokenSource = new CancellationTokenSource();
+                    await client.Videos.Streams.DownloadAsync(
+                        audioStream,
+                        sfd.FileName,
+                        new Progress<double>(download_progress),
+                        _cancelTokenSource.Token
+                    );
+                    rtb_output.AppendText("Finished!" + Environment.NewLine);
+                    progress.Value = 0;
+                    lb_progress.Text = "Ready";
+                }
+                else
+                {
+                    rtb_output.AppendText("Save file dialog canceled operation." + Environment.NewLine);
+                    throw new OperationCanceledException();
+                }
+            }
+            catch (Exception exception)
+            {
+                rtb_output.AppendText(exception.ToString());
+                progress.ForeColor = Color.Red;
+                lb_progress.Text = "Failed";
+            }
+            finally
+            {
+                btn_cancel.Enabled = false;
+                btn_start.Enabled = true;
+            }
+        }
+
+        private void download_progress(double progress)
+        {
+            int downloadProgress = (int)(progress * 100);
+            this.progress.Value = downloadProgress;
+            lb_progress.Text = $"{downloadProgress}%";
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            _cancelTokenSource.Cancel();
+        }
+    }
+}
